d9e7796aa1e7b0dbe9a90d8472a00a4d5f41ce8b
azotamiota <azotamiota@gmail.com>
2025-02-25 21:48:31 +0000

    bugfix vol6

--- scripts/docker_action.sh
+++ scripts/docker_action.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Arguments:
+# $1: Docker command (e.g., "build")
+# $2: Commit SHA (e.g., "abc123")
+# $3: Git ref (e.g., "refs/heads/main")
+# $4: Event name (e.g., "push" or "pull_request")
+# $5: Docker Hub username (e.g., "myusername")
+
 if [[ "$3" == "refs/heads/main" || "$4" == "pull_request" ]]; then
     docker $1 -t "$5/iphone-shop-app:latest" .
 else
