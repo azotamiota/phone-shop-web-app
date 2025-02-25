4fe0ba8c7c3f8310978eca37b29ad0912f768baf
azotamiota <azotamiota@gmail.com>
2025-02-25 21:22:48 +0000

    decouple docker action

---
+++ scripts/docker_action.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [[ "${{ github.ref }}" == "refs/heads/main" || "${{ github.event_name }}" == "pull_request" ]]; then
+    docker $1 -t ${{ vars.DOCKERHUB_USERNAME }}/iphone-shop-app:latest .
+else
+    docker $1 -t ${{ vars.DOCKERHUB_USERNAME }}/iphone-shop-app:$2" .
+fi   
