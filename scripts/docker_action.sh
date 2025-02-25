ec6380111394530e4fcd02475677a1901d58d27e
azotamiota <azotamiota@gmail.com>
2025-02-25 22:01:21 +0000

    refactor action parsing

--- scripts/docker_action.sh
+++ scripts/docker_action.sh
@@ -8,7 +8,13 @@
 # $5: Docker Hub username (e.g., "myusername")
 
 if [[ "$3" == "refs/heads/main" || "$4" == "pull_request" ]]; then
-    docker $1 -t "$5/iphone-shop-app:latest" .
-else
-    docker $1 -t "$5/iphone-shop-app:$2" .
-fi   
+  if [[ "$1" == "build" ]]; then
+    docker build -t "$5/iphone-shop-app:latest" .
+  elif [[ "$1" == "push" ]]; then
+    docker push "$5/iphone-shop-app:latest"
+  fi
+elif [[ "$1" == "build" ]]; then
+    docker build -t "$5/iphone-shop-app:$2" .
+elif [[ "$1" == "push" ]]; then
+    docker push "$5/iphone-shop-app:$2" .
+fi
