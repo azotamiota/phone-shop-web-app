ca788cc52ad2a1fbf106e85df22f4f3b9933b5e5
azotamiota <azotamiota@gmail.com>
2025-02-25 21:24:17 +0000

    bugfix

--- scripts/docker_action.sh
+++ scripts/docker_action.sh
@@ -3,5 +3,5 @@
 if [[ "${{ github.ref }}" == "refs/heads/main" || "${{ github.event_name }}" == "pull_request" ]]; then
     docker $1 -t ${{ vars.DOCKERHUB_USERNAME }}/iphone-shop-app:latest .
 else
-    docker $1 -t ${{ vars.DOCKERHUB_USERNAME }}/iphone-shop-app:$2" .
+    docker $1 -t ${{ vars.DOCKERHUB_USERNAME }}/iphone-shop-app:$2 .
 fi   
